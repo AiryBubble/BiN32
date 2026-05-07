48adc21fdec0e9d32cee166d77ede0eceed17084
AiryBubble <111546646+AiryBubble@users.noreply.github.com>
2026-05-07 13:24:42 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "static-cache-v1";
+const FILES_TO_CACHE = [
+  "/style.css",
+  "/NotoSansJP-Regular.woff2",
+  "/favicon.webp",
+  "/app/index.html",
+  "/app/pwgen/index.html",
+  "/app/qrgen/index.html",
+  "/app/qrgen/qrcode.js",
+  "/app/qrgen/jquery.js",
+  "/app/uidgen/index.html",
+  "/app/hue/index.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
